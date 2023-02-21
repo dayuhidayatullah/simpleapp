49fc4d773f1cd909de05b3b85632844e7a90b474
MAC <mac@192.168.1.9>
2023-02-22 00:25:04 +0700

    initial-project

---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div className="app">
+            <h1 >Test Dulu</h1>
+            <p >Test Dulu</p>
+            <p >Test Dulu</p>
+
+        </div>
+    )
+}
+
+export default Home;
